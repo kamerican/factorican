2190f2f75a4b82a81bdecbb9e20794613eeac4a3
kamerican <44129995+kamerican@users.noreply.github.com>
2019-02-10 19:17:46 -0600

    finish manufacturing recipes

---
+++ factoricanindustry_0.0.1/prototypes/manufacturing/items.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+    {
+        type = "item",
+        name = "fm-quartz",
+        icon = "__factoricanindustry__/graphics/icons/items/fm-quartz.png",
+        icon_size = 32,
+        flags = {"goes-to-main-inventory"},
+        subgroup = "factorican-stone",
+        order = "z",
+        stack_size = 100
+    },
+    
+})
